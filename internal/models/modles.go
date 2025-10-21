570ff1b622e8233a87d5d77a1e504caaac065504
Mohammed <mohamedhishamm50@gmail.com>
2025-10-22 00:48:33 +0300

    Setting up models

--- internal/models/modles.go
+++ internal/models/modles.go
@@ -9,7 +9,7 @@ type Reservation struct {
 	Email     string
 	Phone     string
 }
-
+// Users holds the user data modle
 type Users struct{
 
 	ID int
@@ -20,7 +20,52 @@ type Users struct{
 	AccessLevel int
 	CreateAt time.Time
 	UpdateAt time.Time
+
+
+
+}
+
+// Rooms holds the room data
+type Rooms struct {
+	ID int
+	RoomName string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restrictions holds the restriction data
+type Restrictions struct {
+	ID int
+	RestrictionName string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+// Reservations holds the reservation data
+type Reservations struct {
+	ID int
+	FirstName string
+	LastName string
+	Email string
+	Phone string
+	StartDate time.Time
+	EndDate time.Time
+	RoomID int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room Rooms
+
 	
+}
 
-
+//RoomRestrictions holds the room restriction
+type RoomRestrictions struct {
+	ID int
+	RoomID int
+	ReservationID int
+	RestrictionID int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room Rooms
+	Restriction Restrictions
+	Reservation Reservations
 }
