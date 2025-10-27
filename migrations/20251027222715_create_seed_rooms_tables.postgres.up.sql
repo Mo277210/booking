6411ab5e9f8a2afc0b3f2b54088f1f73d9730537
Mohammed <mohamedhishamm50@gmail.com>
2025-10-28 01:31:31 +0300

    Adding a migration for seeding rooms

---
+++ migrations/20251027222715_create_seed_rooms_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (rooms_name,created_at,updated_at) VALUES
+	 ('General''s Quarots','2025-11-18 00:00:00','2020-11-18 00:00:00'),
+	 ('Major''s Suite','2021-07-04 00:00:00','2021-07-08 00:00:00');
