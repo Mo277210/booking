72d3a137122f3ccf64542b152e45fd13778acadb
Mohammed <mohamedhishamm50@gmail.com>
2025-10-28 01:36:04 +0300

    Adding a migration for seeding restrictions

---
+++ migrations/20251027223244_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restrictions_name,created_at,update_at) VALUES
+	 ('Reserviaton','2025-11-18 00:00:00.000','2025-11-18 00:00:00.000'),
+	 ('owner block','2020-11-19 00:00:00.000','2020-11-19 00:00:00.000');
